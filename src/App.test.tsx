5ca2503115c8cf61ac305614c2b21364c2308a72
Adam Jones <domdomegg+git@gmail.com>
2021-11-13 01:33:54 +0000

    Add CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// We can mock our translations if we wanted to
+// import t from './i18n/browser'
+
+test('renders nested works text', () => {
+  // We can mock our translations if we wanted to
+  // t.nested.works = jest.fn().mockReturnValue('Nested works')
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nestedWorksMessage = screen.getByText(/nested works/i);
+  expect(nestedWorksMessage).toBeInTheDocument();
 });
